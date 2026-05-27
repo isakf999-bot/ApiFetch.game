af17eb79493c7ded08b9acb3ebcf03cf8c22526e
foppa <isakf999@gmail.com>
2026-05-27 14:51:51 +0200

    fixed that the website opens to login page and not game itself

--- React-Api-Game/src/App.jsx
+++ React-Api-Game/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import IpFetcher from './Components/IpFetcher'
+import Login from './Components/Login'
+import Register from './Components/Register'
 import './App.css'
 
 function App() {
+  const [page, setPage] = useState('login')
+
   return (
     <div>
-      <IpFetcher />
+      {page === 'login' && (
+        <Login
+          goToRegister={() => setPage('register')}
+          goToGame={() => setPage('game')}
+        />
+      )}
+
+      {page === 'register' && (
+        <Register goToLogin={() => setPage('login')} />
+      )}
+
+      {page === 'game' && <IpFetcher />}
     </div>
   )
 }
